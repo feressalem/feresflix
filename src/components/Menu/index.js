96137f8b8921ee48001e12ee30135928ccde71f0
Feres Azevedo Salem <feres_salem@hotmail.com>
2020-07-29 10:29:57 -0300

    Commit meio da aula 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import Logo from '../../assets/img/Logo.png'
-import './Menu.css'
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
 import Button from '../Button';
 
 function Menu(){
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="Feresflix Logo"></img>
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo Vídeo
             </Button>
         </nav>
